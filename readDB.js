8f1baf66f4659271986904c9b7429d374010fd31
Eliav Cohen <eliav.s.cohen@gmail.com>
2021-06-29 20:31:55 +0300

    lookup code

--- readDB.js
+++ readDB.js
@@ -1,3 +1,14 @@
 var fs = require('fs');
 var dictionary = JSON.parse(fs.readFileSync('resources/db.json'));
-console.log(dictionary.length);
+var readline = require("readline");
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+function query() {
+    rl.question('What are you looking for?', function (name) {
+        console.log(dictionary.filter(function (dic) { return dic.lookup === name; })[0]);
+        query();
+    });
+}
+query();
--- readDB.ts
+++ readDB.ts
@@ -1,4 +1,19 @@
 const fs = require('fs');
 
 const dictionary = JSON.parse(fs.readFileSync('resources/db.json'));
-console.log(dictionary.length);
+
+const readline = require("readline");
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+
+function query() {
+	rl.question('What are you looking for?', function (name) {
+		console.log(dictionary.filter(dic => dic.lookup === name)[0]);
+		query();
+	});
+}
+
+query();
+// 71747 words
